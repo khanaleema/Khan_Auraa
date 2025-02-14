17b5e36ca821798c9cc09ea80d48a0dfa79306b0
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2025-02-14 18:18:44 +0500

    Initial commit

--- src/app/(user)/signin/page.tsx
+++ src/app/(user)/signin/page.tsx
@@ -15,35 +15,36 @@ const SignInPage = async () => {
   }
   return (
     <Container className="py-20 flex flex-col items-center justify-center">
-      <div className="w-[500px] bg-bgLight p-10 rounded-lg shadow-sm shadow-darkOr/50">
+      <div className="max-w-md w-full bg-bgLight p-6 sm:p-10 rounded-lg shadow-sm shadow-darkOr/50">
         <div className="mb-5">
           <h2 className="text-xl font-bold">Oauth sign in</h2>
-          <div className="flex items-center gap-3 mt-2">
+          <div className="flex flex-col sm:flex-row items-center gap-3 mt-2">
             <form
               action={async () => {
                 "use server";
                 await signIn("google", { redirectTo: "/" });
               }}
-              className="flex items-center gap-1 border border-blue-500 font-semibold bg-blue-50 px-2 py-1.5 rounded-md hover:bg-blue-800 hover:text-white duration-300 ease-in-out"
+              className="flex items-center gap-2 border border-blue-500 font-semibold bg-blue-50 px-4 py-2 rounded-md hover:bg-blue-800 hover:text-white duration-300 ease-in-out min-w-[160px] text-center"
             >
-              <Image src={googleImage} alt="googleImage" className="w-8" />
-              <button type="submit">Signin with Google</button>
+              <Image src={googleImage} alt="googleImage" className="w-6 sm:w-8" />
+              <button type="submit" className="w-full">Signin with Google</button>
             </form>
             <form
               action={async () => {
                 "use server";
-                await signIn("google", { redirectTo: "/" });
+                await signIn("github", { redirectTo: "/" });
               }}
-              className="flex items-center gap-1 border border-slate-500 font-semibold bg-slate-50 px-2 py-1.5 rounded-md hover:bg-slate-200 duration-300 ease-in-out"
+              className="flex items-center gap-2 border border-slate-500 font-semibold bg-slate-50 px-4 py-2 rounded-md hover:bg-slate-200 duration-300 ease-in-out min-w-[160px] text-center"
             >
-              <Image src={githubImage} alt="githubImage" className="w-8" />
-              <button type="submit">Signin with Github</button>
+              <Image src={githubImage} alt="githubImage" className="w-6 sm:w-8" />
+              <button type="submit" className="w-full">Signin with Github</button>
             </form>
           </div>
         </div>
         <SignInForm />
       </div>
     </Container>
+
   );
 };
 
